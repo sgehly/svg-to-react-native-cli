f67d552a30ccae830199b417805c88c42a8a6cd6
Ben Thornburg <ben@alchemy.codes>
2018-10-05 15:19:53 -0400

    reformat component as class with defaults and proptypes

--- src/generateComponent.js
+++ src/generateComponent.js
@@ -6,8 +6,9 @@
  * @param  string componentName The name of the component without extension
  * @return string               The parsed component string
  */
-module.exports = (svgOutput, componentName) => `
-import React from 'react';
+module.exports = (svgOutput, componentName, width, height) => `
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import {
   Svg,
   Circle,
@@ -24,15 +25,30 @@ import {
   Text,
   Use,
   Defs,
-  Stop
+  Stop,
+  ClipPath
 } from 'react-native-svg';
 
-export default function ${componentName}(props) {
-  return (
+export default class ${componentName} extends Component {
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    opacity: PropTypes.number,
+    style: PropTypes.any,
+  }
+
+  static defaultProps = {
+    width: ${width},
+    height: ${height},
+    opacity: 1,
+    style: {},
+  };
+
+  render() {
 ${svgOutput
   .split('\n')
   .map(line => `    ${line}`)
   .join('\n')}
-  );
+  }
 }
 `;

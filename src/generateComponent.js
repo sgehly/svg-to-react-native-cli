4baefd8fc8c9317070274603442bc13764795d6d
Ben Thornburg <ben@alchemy.codes>
2018-10-17 13:43:08 -0400

    add colors prop to PropTypes

--- src/generateComponent.js
+++ src/generateComponent.js
@@ -35,6 +35,7 @@ export default class ${componentName} extends Component {
     height: PropTypes.number,
     opacity: PropTypes.number,
     style: PropTypes.any,
+    colors: PropTypes.array,
   };
 
   static defaultProps = {

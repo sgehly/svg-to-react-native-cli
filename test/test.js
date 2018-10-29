a2d677eb23b7cb78da9a2c6700bfcba27078bebe
Ben Thornburg <ben@alchemy.codes>
2018-10-29 15:44:42 -0400

    test: setup snapshot test
    Generate an SVG and compare it with a snapshot. Also added a script to update the snapshot.
    
    BREAKING CHANGE: npm test now runs mocha after generating an SVG

--- test/test.js
+++ test/test.js
@@ -1,6 +1,12 @@
-var assert = require('assert');
-describe('First Test', () => {
-    it('should return true', () => {
-        assert.equal(1, 1);
+const assert = require('assert');
+const fs = require('fs');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Compare generated file', () => {
+    it('should match the snapshot', () => {
+        let generatedFile = fs.readFileSync('src/test-svgs/TestSvg.js');
+        let snapshotFile = fs.readFileSync('src/test-svgs/snapshots/TestSvg.js');
+        expect(generatedFile).to.deep.equal(snapshotFile);
     });
 });
